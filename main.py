6d2a187ed1a9938ee69f8cf3de9d9b9ea6df033f
foresle <foresle@tutanota.com>
2022-02-03 13:45:54 +0200

    Fix issues when the terminal is less than the length of the term

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from packages import *
+import os
 
 
 def run():
@@ -6,18 +7,23 @@ def run():
     left_col: list = AsciiLogo().__list__().copy()
     right_col: list = [' ' for i in range(2)] + SessionInfo().__list__().copy() + MemoryInfo().__list__().copy()
 
+    ts: os.terminal_size = os.get_terminal_size()
+
     output_list: list = []
 
     if len(left_col) > len(right_col) or len(left_col) == len(right_col):
         for line in range(0, len(right_col)):
-            output_list.append(f'{left_col[line]}\t{right_col[line]}')
+            output_list.append(f'{left_col[line]}  {right_col[line]}')
 
         for line in range(len(right_col) - 1, len(left_col)):
             output_list.append(f'{left_col[line]}')
 
     else:
         for line in range(len(left_col) - 1, len(right_col)):
-            output_list.append('' * len(left_col[0]) + f'{right_col[line]}')
+            output_list.append('' * len(left_col[0]) + f'  {right_col[line]}')
+
+    for line_index in range(0, len(output_list)):
+        output_list[line_index] = output_list[line_index][:ts.columns]
 
     output_string: str = '\n'.join(output_list)
     addons_output_string: str = ''
